e43f9e88ac84c1b584e77cbec777e1e23e78cc0a
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-02-01 20:06:53 -0800

    Eureka Service Registry Set up complete

---
+++ eureka-server/src/test/java/com/flowcent/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowcent.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ notification-service/src/test/java/com/flowcent/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowcent.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
