d458836ae3b5b3691eb27b689efd3b3f0dc8b774
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-02-02 15:13:36 -0800

    Kong API gateway is successfully working and redirecting the routes to different microservices

--- account-service/src/main/java/com/flowcent/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/flowcent/accountservice/AccountServiceApplication.java
@@ -2,9 +2,10 @@ package com.flowcent.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-
+@RequestMapping("/accounts")
 public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
