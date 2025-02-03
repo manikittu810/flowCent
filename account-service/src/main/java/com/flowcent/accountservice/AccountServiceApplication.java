7cffb60f3bf86df621c0d2b793a2085ecb5ef717
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-02-02 23:01:36 -0800

    Eureka Service Registry working with local host, not with Kong API gateway and Docker

--- account-service/src/main/java/com/flowcent/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/flowcent/accountservice/AccountServiceApplication.java
@@ -2,10 +2,8 @@ package com.flowcent.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping("/accounts")
 public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
