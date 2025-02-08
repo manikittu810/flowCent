82853281883bae188bbbee0a60a3afa3d0a57107
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-02-08 01:19:43 -0800

    added account-service as a module for security as request validator for all  microservices

---
+++ auth-service/src/test/java/com/flowcent/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowcent.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
